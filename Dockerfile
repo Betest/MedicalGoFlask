aa4468496281c221bf989927cabae06e0e00d749
jtobon1 <jtobon@cidenet.com.co>
2021-04-11 20:10:04 -0500

    se actualiza repo (mejoras docker compose)

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,4 @@ COPY . .
 
 RUN pip3 install -r requirements.txt
 
-
 CMD ["flask", "run"]
--- src/app.py
+++ src/app.py
@@ -3,30 +3,38 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="mysql",
-  user="root",
-  passwd="admin",
-  database="appointmentapp"
-)
 
-app = Flask(__name__)
-
-#settings
-app.secret_key = 'secretkey'
+def connect_db():
+    """Connects to the specific database."""
+    mydb = mysql.connector.connect(
+        user="root",
+        passwd="admin",
+        host="mysql",
+        port='3306',
+        database="appointmentapp"
+    )
+    return mydb
 
 #form = AppointmentForm(request.form)
 
 
+# settings
+app = Flask(__name__)
+app.secret_key = 'secretkey'
 
+mydb = connect_db()
 # Rutas
+
+
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+
 @app.route('/home')
 def root():
     return render_template('home.html')
+
 
 @app.route('/')
 def home():
@@ -35,15 +43,15 @@ def home():
     mycursor.execute(sql)
     res = mycursor.fetchall()
 
-    return render_template('home.html', appointments = res)
+    return render_template('home.html', appointments=res)
+
 
 @app.route('/createappointment')
 def createappointment():
     return render_template('create-appointment.html')
 
+
 # Mamejo de peticiones Add Appointment
-
-
 @app.route('/addappointment', methods=['POST'])
 def addappointment():
     if request.method == 'POST':
@@ -58,20 +66,22 @@ def addappointment():
 
         mycursor = mydb.cursor()
         sql = f"INSERT INTO appointments (firstName, lastName, ident, date, city, neighborhood, mobile, dateAppointment) VALUES('{firstName}','{lastName}','{ident}','{date}','{city}','{neighborhood}','{mobile}','{dateAppointment}')"
-        mycursor.execute(sql)        
+        mycursor.execute(sql)
         mydb.commit()
 
         flash('Cita agregada correctamente!')
 
         return redirect(url_for('home'))
 
+
 @app.route('/editappointment/<id>')
 def editappointment(id):
-    mycursor = mydb.cursor()    
+    mycursor = mydb.cursor()
     mycursor.execute(f'SELECT * FROM appointments WHERE id = {id}')
     data = mycursor.fetchall()
     print(data)
-    return render_template('edit-appointment.html', appointment = data[0])
+    return render_template('edit-appointment.html', appointment=data[0])
+
 
 @app.route('/updateappointment/<id>', methods=['POST'])
 def updateappointment(id):
@@ -85,7 +95,8 @@ def updateappointment(id):
         mobile = request.form['mobile']
         dateAppointment = request.form['dateAppointment']
         mycursor = mydb.cursor()
-        mycursor.execute("""UPDATE appointments SET firstName = %s, lastName = %s, ident = %s, date = %s, city = %s, neighborhood = %s, mobile = %s, dateAppointment = %s WHERE id = %s""", (firstName, lastName, ident, date, city, neighborhood, mobile, dateAppointment, id))
+        mycursor.execute("""UPDATE appointments SET firstName = %s, lastName = %s, ident = %s, date = %s, city = %s, neighborhood = %s, mobile = %s, dateAppointment = %s WHERE id = %s""",
+                         (firstName, lastName, ident, date, city, neighborhood, mobile, dateAppointment, id))
         flash('Cita actualizada correctamente!')
     return redirect(url_for('home'))
 
@@ -99,12 +110,10 @@ def deleteppointment(id):
 
     flash('Cita eliminada correctamente!')
 
-    return redirect (url_for('home'))
+    return redirect(url_for('home'))
 
 
-
-    
 # Ejecutar la app en el server / en modo debug
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000 ,debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
